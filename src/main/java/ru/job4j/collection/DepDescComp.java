85ae3732095fb639aad0dcbb33e33311108f2fff
user <none>
2020-10-19 13:33:44 +0500

    Отсортировать департаменты [#282428]

--- src/main/java/ru/job4j/collection/DepDescComp.java
+++ src/main/java/ru/job4j/collection/DepDescComp.java
@@ -9,25 +9,9 @@ import java.util.List;
 public class DepDescComp implements Comparator<String> {
     @Override
     public int compare(String o1, String o2) {
-        List<String> left = new ArrayList<>();
-        List<String> right = new ArrayList<>();
-        left.addAll(Arrays.asList(o1.split("/")));
-        right.addAll(Arrays.asList(o2.split("/")));
-        if (left.size() == 0 && right.size() == 0) {
-            return 0;
-        }
-        int minsize = Math.min(left.size(), right.size());
-        for (int i = 0; i < minsize; i++) {
-            int cmp = left.get(i).compareTo(right.get(i));
-            if (cmp != 0) {
-                return cmp;
-            } else if (i == minsize - 1) {
-                return Integer.compare(left.size(), right.size());
-            }
-// else {
-//                return Integer.compare(left, )
-//            }
-        }
-        return 0;
+        String[] left = o1.split("/");
+        String[] right = o2.split("/");
+        int cmp = left[0].compareTo(right[0]);
+        return cmp == 0 ? o1.compareTo(o2) : cmp;
     }
 }
